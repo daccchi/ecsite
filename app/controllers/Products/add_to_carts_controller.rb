703afc0e40d1a29392a71014e07d0fc6e5b693b7
taro.kirameki <taro.kirameki@techacademy.jp>
2021-05-24 12:30:26 +0000

    for heroku

--- app/controllers/Products/add_to_carts_controller.rb
+++
@@ -1,8 +0,0 @@
-class Products::AddToCartsController < Products::ApplicationController
-    def create
-      cart = current_customer.prepare_cart
-      product = Product.find(params[:product_id])
-      cart.cart_products.create!(product_id: product.id)
-      redirect_to cart_path
-    end
-end
