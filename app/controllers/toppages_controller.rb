987a3171170595b8f85d9b2ebfba68fdccfe5d76
taro.kirameki <taro.kirameki@techacademy.jp>
2021-05-26 06:04:39 +0000

    for heroku

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,8 @@
 class ToppagesController < ApplicationController
   def index
+    @product1 = Product.find(1)
+   @product2 = Product.find(2)
+   @product3 = Product.find(3)
+   @product4 = Product.find(4)
   end
 end
