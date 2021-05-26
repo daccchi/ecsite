0fcf0c18c5517f92f4633a30bda26b6ee69a1e7e
taro.kirameki <taro.kirameki@techacademy.jp>
2021-05-26 03:26:36 +0000

    for heroku

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,9 @@
 class ProductsController < ApplicationController
   def index
-   # @product = Product.find(1)
+   @product1 = Product.find(1)
+   @product2 = Product.find(2)
+   @product3 = Product.find(3)
+   @product4 = Product.find(4)
   end
 
   def show
