8fd0b179444ecb580247637a1d3b0ce416ec9321
taro.kirameki <taro.kirameki@techacademy.jp>
2021-05-29 05:00:51 +0000

    for heroku

--- config/application.rb
+++ config/application.rb
@@ -11,13 +11,21 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
+
+#require "dotenv/rails-now"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
+Bundler.require(*Rails.groups)
+if ['development', 'test'].include? ENV['RAILS_ENV']
+  Dotenv::Railtie.load
+end
+
+
+
+#Dotenv::Railtie.load
 
 
 module Ecsite
