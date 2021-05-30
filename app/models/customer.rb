361cbdde1d53b893bc5eec264384e069abf74690
taro.kirameki <taro.kirameki@techacademy.jp>
2021-05-30 14:18:28 +0000

    for heroku

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,8 +8,8 @@ class Customer < ApplicationRecord
     before_save { self.email.downcase! }
     validates :family_name, presence: true, length: { maximum: 50 }
     validates :given_name, presence: true, length: { maximum: 50 }
-    validates :family_kana, presence: true, length: { maximum: 50 }
-    validates :given_kana, presence: true, length: { maximum: 50 }
+   # validates :family_kana, presence: true, length: { maximum: 50 }
+   # validates :given_kana, presence: true, length: { maximum: 50 }
     validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
@@ -21,12 +21,12 @@ class Customer < ApplicationRecord
     #validates :birthday_month, presence: true, length: { maximum: 3}, numericality: { only_integer: true }
     #validates :birthday_day, presence: true, length: { maximum: 3 }, numericality: { only_integer: true }
     
-     validates :zip, presence: true, length: { maximum: 7 }, numericality: { only_integer: true }
-      validates :pref, presence: true, length: { maximum: 10 }
+    # validates :zip, presence: true, length: { maximum: 7 }, numericality: { only_integer: true }
+    #  validates :pref, presence: true, length: { maximum: 10 }
       # validates :addr, presence: true, length: { maximum: 10 }
        # validates :addr2, presence: true, length: { maximum: 10 }
         
-    validates :tel, presence: true, length: { maximum: 15 }, numericality: { only_integer: true }
+    #validates :tel, presence: true, length: { maximum: 15 }, numericality: { only_integer: true }
        
     has_secure_password
     has_one :cart, dependent: :destroy
