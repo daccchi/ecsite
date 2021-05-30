60132a14535dc7dbe9c102ea06de5b56654de327
taro.kirameki <taro.kirameki@techacademy.jp>
2021-05-30 09:18:50 +0000

    for heroku

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -9,11 +9,11 @@ class Customer < ApplicationRecord
                     uniqueness: { case_sensitive: false }
                     
                     
-    validates :sex, presence: true, length: { maximum: 3 }
+    #validates :sex, presence: true, length: { maximum: 3 }
     
-    validates :birthday_year, presence: true, length: { maximum: 4 }, numericality: { only_integer: 4 }
-    validates :birthday_month, presence: true, length: { maximum: 2 }, numericality: { only_integer: 2 }
-    validates :birthday_day, presence: true, length: { maximum: 2 }, numericality: { only_integer: 2 }
+    #validates :birthday_year, presence: true, length: { maximum: 15 }, numericality: { only_integer: true }
+    #validates :birthday_month, presence: true, length: { maximum: 3}, numericality: { only_integer: true }
+    #validates :birthday_day, presence: true, length: { maximum: 3 }, numericality: { only_integer: true }
     
      validates :zip, presence: true, length: { maximum: 7 }, numericality: { only_integer: true }
       validates :pref, presence: true, length: { maximum: 10 }
